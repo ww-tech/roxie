20265166624473962dcdd3f2741c228faf64fee0
James Shvarts <jshvarts@gmail.com>
2019-01-17 14:33:40 -0500

    Migrate to AndroidX (#4)
    1. Migrate to AndroidX
    2. Extract dependency versions common between sample and library into `ext`

--- roxie/src/main/kotlin/com/ww/roxie/BaseViewModel.kt
+++ roxie/src/main/kotlin/com/ww/roxie/BaseViewModel.kt
@@ -15,10 +15,10 @@
 */
 package com.ww.roxie
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MediatorLiveData
-import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.PublishSubject
 
--- sample/src/main/kotlin/com/ww/roxiesample/presentation/notedetail/NoteDetailViewModelFactory.kt
+++ sample/src/main/kotlin/com/ww/roxiesample/presentation/notedetail/NoteDetailViewModelFactory.kt
@@ -15,8 +15,8 @@
 */
 package com.ww.roxiesample.presentation.notedetail
 
-import android.arch.lifecycle.ViewModel
-import android.arch.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.ww.roxiesample.domain.DeleteNoteUseCase
 import com.ww.roxiesample.domain.GetNoteDetailUseCase
 
